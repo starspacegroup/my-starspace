db4aeb60d440515011137080a3912618a7e2408f
davis9001 <monaghan.david@gmail.com>
2025-03-11 21:50:22 -0400

    init

---
+++ src/lib/server/sessionHandler.ts
@@ -0,0 +1,40 @@
+import type { APIUser, RESTPostOAuth2AccessTokenResult } from 'discord-api-types/v10';
+import type { FullUser } from 'src/interfaces';
+import crypto from 'node:crypto';
+
+const sessionUsers = new Map<string, FullUser>();
+
+export function setSession(userData: APIUser, tokenGrantData: RESTPostOAuth2AccessTokenResult) {
+
+  // Creating a new session ID that will be used as a cookie to authenticate the user in 
+  const newSessionID = crypto.randomBytes(32).toString('hex');
+  const fullUser: FullUser = { ...userData, ...tokenGrantData };
+
+  sessionUsers.set(newSessionID, fullUser);
+
+  setTimeout(() => {
+    deleteSession(newSessionID);
+  }, 1000 * 60 * 10); //  10 minutes
+
+  return newSessionID;
+}
+
+export function fetchSession(sessionId: string) {
+  const s = sessionUsers.get(sessionId);
+
+  if (s) {
+    delete s.access_token;
+    delete s.refresh_token;
+    delete s.expires_in;
+    delete s.scope;
+    delete s.token_type;
+
+    return s as APIUser;
+  }
+
+  return null;
+}
+
+export function deleteSession(sessionId: string) {
+  sessionUsers.delete(sessionId);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  export let title = "My *Space";
+  export let subtitle = "Productivity through public accountability.";
+</script>
+
+<section class="flex flex-col items-center justify-center min-h-screen text-center px-6">
+  <div class="max-w-2xl">
+    <h1 class="text-4xl font-bold  sm:text-6xl">{title}</h1>
+    <p class="mt-4 text-lg  sm:text-xl">{subtitle}</p>
+    <div class="mt-6">
+      <button class="" disabled>
+        Login
+      </button>
+    </div>
+  </div>
+</section>
