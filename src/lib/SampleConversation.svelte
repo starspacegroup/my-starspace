ffeda2196642dff41488118c7700e34d656b0688
davis9001 <monaghan.david@gmail.com>
2025-03-16 08:48:53 -0400

    slightly improve sample visuals

--- src/lib/SampleConversation.svelte
+++ src/lib/SampleConversation.svelte
@@ -16,14 +16,14 @@
   const userAvatar = 'https://cdn.discordapp.com/avatars/293484886726279168/056525646c5d0a955f276c80e8700815.webp?size=80';
 
   const conversation = [
-  { user: `My *Space`, messageBody: `Starting 25-minute timebox: "Creating a Software Feature".`, bot: true, time: `<t:1741961887:t>` },
-  { user: `David`, messageBody: `Here's the mockup image for my idea:`, time: `<t:1741962007:t>` },
-    { user: `David`, messageBody: `<img src="/sample-screenshot.png" alt="" class="">`, time: `<t:1741962247:t>` },
-      { user: `David`, messageBody: `For styling, I'll set up Tailwind CSS because it makes things easier to style quickly.`, time: `<t:1741962127:t>` },
-  { user: `David`, messageBody: `Now I'll add a basic page with a form for user input.`, time: `<t:1741962187:t>` },
-  { user: `David`, messageBody: `Time to test things out in development mode: \`npm run dev\`.`, time: `<t:1741962307:t>` },
-  { user: `David`, messageBody: `Looks good so far. I'm going to push this to GitHub so I have a backup.`, time: `<t:1741962367:t>` },
-  { user: `My *Space`, messageBody: `Time's up! 25-minute session completed. Take a short break.`, bot: true, time: `<t:1741962427:t>` }
+  { user: `My *Space`, messageBody: `Starting 25-minute timebox: "Creating a Software Feature".`, bot: true, time: `<t:1741961887:R>` },  
+  { user: `David`, messageBody: `Here's the mockup image for my idea:`, time: `<t:1741962007:R>` },
+    { user: `David`, messageBody: `<img src="/sample-screenshot.png" alt="" class="">`, time: `<t:1741962247:R>` },
+      { user: `David`, messageBody: `For styling, I'll set up Tailwind CSS because it makes things easier to style quickly.`, time: `<t:1741962127:R>` },
+  { user: `David`, messageBody: `Now I'll add a basic page with a form for user input.`, time: `<t:1741962187:R>` },
+  { user: `David`, messageBody: `Time to test things out in development mode: \`npm run dev\`.`, time: `<t:1741962307:R>` },
+  { user: `David`, messageBody: `Looks good so far. I'm going to push this to GitHub so I have a backup.`, time: `<t:1741962367:R>` },
+  { user: `My *Space`, messageBody: `Time's up! 25-minute session completed. Take a short break.`, bot: true, time: `<t:1741962427:R>` }
 ];
 
   function startSimulation() {
@@ -70,20 +70,24 @@
   }
 </style>
 
-<div class="chat-container">
+<div class="chat-container text-left">
   {#each messages as msg}
     <div class="message {msg.bot ? 'bot' : 'user'}">
-      <div class="timestamp">
-        <strong>{msg.user}</strong>
-        <span class="font-extralight text-foreground/50">
-          {formatDiscordTimestamp(msg.time)}
-        </span>
-      </div>
-      <div>
-        <img src={msg.bot ? botAvatar : userAvatar} alt="">
-      </div>
-      <div>
-        {@html msg.messageBody}
+      <div class="flex">
+        <div class="w-20 min-w-20">
+          <img src={msg.bot ? botAvatar : userAvatar} alt="" class="avatar w-20 rounded-full" />
+        </div>
+        <div class="text-left">
+          <div class="timestamp">
+            <strong>{msg.user}</strong>
+            <span class="font-extralight text-foreground/50">
+              {formatDiscordTimestamp(msg.time)}
+            </span>
+          </div>
+          <div>
+            {@html msg.messageBody}
+          </div>
+        </div>
       </div>
     </div>
   {/each}
