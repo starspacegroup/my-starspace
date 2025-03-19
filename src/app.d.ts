1de981fc7ae519ebee7e49c1a03a61c50c777dde
davis9001 <monaghan.david@gmail.com>
2025-03-19 01:09:57 -0400

    Add some discord endpoints

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
+    namespace App {
         interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
+            env?: {
+                TIMEBOX_KV: KVNamespace;
+            };
+            cf: CfProperties;
+            ctx: ExecutionContext;
         }
     }
 }
 
-export {};
+export { };
