54e5beb60cb4c7915afeb659d953da566ead8a7e
davis9001 <monaghan.david@gmail.com>
2025-03-22 11:56:52 -0400

    Add some details and logout button

--- src/routes/user-dashboard/+page.svelte
+++ src/routes/user-dashboard/+page.svelte
@@ -1,3 +1,16 @@
+<script>
+  import { page } from '$app/state';
+  import { SignOut } from '@auth/sveltekit/components'
+
+</script>
+
+
 <main>
   <h1>User Dashboard</h1>
+  {#if page.data.session}
+    <p>Signed in as {page.data.session.user?.name}</p>
+    <p>User id: {page.data.session.user?.id}</p>
+    <img src={page.data.session.user?.image} alt="">
+    <SignOut signOutPage="user-auth/logout" options={{redirectTo: "/"}} />
+  {/if}
 </main>
