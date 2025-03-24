14d7dbff801091855bf1e0c60119c4e02bc867ef
davis9001 <monaghan.david@gmail.com>
2025-03-24 11:21:49 -0400

    Add description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
   import SampleConversation from "$lib/SampleConversation.svelte";
-  import { signIn, signOut } from "@auth/sveltekit/client"
-  import { page } from "$app/stores";
-  import { SignIn, SignOut } from "@auth/sveltekit/components"
+  import { page } from "$app/state";
+  import { SignIn } from "@auth/sveltekit/components"
   import Icon from "@iconify/svelte"
 
   export let title = "My *Space";
-  export let subtitle = "Productivity through public accountability.";
+  export let subtitle = "Post to your personal website from Discord.";
+  export let description = `Exclusive to <a href="https://discord.gg/6JXhPBAUWU" class="text-blue-500">*Space</a> members.`;
 </script>
 
 <main class="sm:flex items-center justify-center min-h-screen text-center px-6">  
@@ -14,15 +14,16 @@
     <div class="max-w-2xl">
       <h1 class="text-4xl font-bold sm:text-6xl">{title}</h1>
       <p class="mt-4 text-lg sm:text-xl">{subtitle}</p>
-      {#if $page.data.session}
-        <p>Signed in as {$page.data.session.user?.name}</p>
+      <p class="mt-4 text-lg sm:text-xl">{@html description}</p>
+      {#if page.data.session}
+        <p>Signed in as {page.data.session.user?.name}</p>
          <a href="/user-dashboard" class="text-blue-500 hover:underline">My Dashboard</a>
       {:else}
-        <SignIn provider="discord" signInPage="user-auth/login" options={{redirect: false, redirectTo: "/user-dashboard"}}>
+        <SignIn provider="discord" signInPage="user-auth/login" options={{redirect: false, redirectTo: "/user-dashboard"}} className="my-3">
           <div slot="submitButton" class="flex space-x-2 items-center">
             <Icon icon="ph:discord-logo-fill" />
-            <span>Login</span>
-          </div>  
+            <span>Discord Login</span>
+          </div>
         </SignIn>
       {/if}
     </div>
