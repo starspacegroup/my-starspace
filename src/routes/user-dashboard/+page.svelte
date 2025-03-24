808536d44ec9323363545686c971ba07ae113e04
davis9001 <monaghan.david@gmail.com>
2025-03-24 11:24:31 -0400

    tidying

--- src/routes/user-dashboard/+page.svelte
+++ src/routes/user-dashboard/+page.svelte
@@ -5,25 +5,25 @@
   import Icon from '@iconify/svelte'
 
   const userCompletedSignup = false;
-  const userSignedIn = page.data.session;
+  const userSignedIn = page.data.session?.user || false;
 </script>
 
-<main class="min-h-screen min-w-screen flex items-center">
-  <div class="max-w-3xl mx-auto p-6 text-white bg-[#2b2d31] rounded-md shadow-lg">
+<main class="min-h-screen w-screen flex items-center min-w-xs">
+  <div class="sm:max-w-3xl mx-auto p-6 text-white bg-[#2b2d31] rounded-md shadow-lg">
     <h1 class="text-2xl font-bold mb-4">User Settings</h1>
     
-    {#if userSignedIn && userSignedIn.user?.image}
+    {#if userSignedIn && userSignedIn.image}
     <div class="flex items-center gap-4 bg-[#313338] p-4 rounded-md mb-6">
       <img 
-      src={userSignedIn.user.image} 
-      alt={`${userSignedIn.user.username}'s avatar`} 
+      src={userSignedIn.image} 
+      alt={`${userSignedIn.username}'s avatar`} 
       class="w-24 h-24 rounded-full border border-gray-600"
       />
       <div class="w-full">
-        <p class="text-lg font-medium">Discord name: {userSignedIn.user.name}</p>
-        <p class="text-lg font-medium">Discord username: {userSignedIn.user.username}</p>
-        <p class="text-lg font-medium">Discord ID: {userSignedIn.user.id?.toString()}</p>
-        <p class="text-lg font-medium">Discord email: {userSignedIn.user.email}</p>
+        <p class="text-lg font-medium">Discord name: {userSignedIn.name}</p>
+        <p class="text-lg font-medium">Discord username: {userSignedIn.username}</p>
+        <p class="text-lg font-medium">Discord ID: {userSignedIn.id?.toString()}</p>
+        <p class="text-lg font-medium">Discord email: {userSignedIn.email}</p>
         <div class="text-right">
           <SignOut provider="discord" signOutPage="user-auth/logout" options={{redirect: false, redirectTo: "/"}}>
             <div slot="submitButton" class="flex space-x-2 items-center">
@@ -43,13 +43,13 @@
         <label class="block mb-2 text-sm font-medium text-gray-300" for="username_text">Choose a url slug:</label>
         <div class="flex items-center bg-[#1e1f22] rounded-md">
           <span class="text-gray-400 italic pl-3">https://my.starspace.group/</span>
-          <input type="text" value={userSignedIn.user?.username ?? 'username'} placeholder="Username" class="bg-transparent text-white ml-2 flex-1 ring-green-500 ring-1 rounded focus:ring-0 p-1" id="username_text" />
+          <input type="text" value={userSignedIn.username ?? 'username'} placeholder="Username" class="bg-transparent text-white ml-2 flex-1 ring-green-500 ring-1 rounded focus:ring-0 p-1" id="username_text" />
         </div>
       </div>
         
       <div class="bg-yellow-500/10 border-l-4 border-yellow-500 p-4 rounded-md flex items-center space-x-2">
         <Icon icon="typcn:info" class="text-yellow-500" />
-        <p class="text-yellow-400">By registering you are agreeing to our <a href="/terms-of-service" target="_blank" class="text-blue-400 hover:underline">Terms of Service</a> and <a href="/privacy-policy" target="_blank" class="text-blue-400 hover:underline">Privacy Policy</a>.</p>
+        <p class="text-yellow-400">By registering you are agreeing to the My *Space <a href="/terms-of-service" target="_blank" class="text-blue-400 hover:underline">Terms of Service</a> and <a href="/privacy-policy" target="_blank" class="text-blue-400 hover:underline">Privacy Policy</a>.</p>
       </div>
             
       <button class="mt-4 w-full px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md">Register Slug</button>
@@ -59,7 +59,7 @@
       <label class="block mb-2 text-sm font-medium text-gray-300" for="username_text">Change your url slug:</label>
       <div class="flex items-center bg-[#1e1f22] p-2 rounded-md">
         <span class="text-gray-400 italic">https://my.starspace.group/</span>
-        <input type="text" value={userSignedIn.user?.username ?? 'username'} placeholder="Username" class="bg-transparent text-white ml-2 flex-1 ring-1 rounded focus:ring-0 p-1" id="username_text" />
+        <input type="text" value={userSignedIn.username ?? 'username'} placeholder="Username" class="bg-transparent text-white ml-2 flex-1 ring-1 rounded focus:ring-0 p-1" id="username_text" />
       </div>
       <button class="mt-4 w-full px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md">Update Slug</button>
     </div>
